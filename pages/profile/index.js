8069811520b3637384e33db30f20e29b616c2a40
MustHaveLove <wodnjs6159@gmail.com>
2022-10-13 15:36:07 +0900

    first

---
+++ pages/profile/index.js
@@ -0,0 +1,15 @@
+//rfc
+
+export default function profile() {
+  const name = "류재원";
+  const email = "wodnjs6159@naver.com";
+  const homepage = "https://steamcommunity.com/profiles/76561198144472168";
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Name : {name}</p>
+      <p>Email : {email}</p>
+      <p>Homepage : {homepage}</p>
+    </div>
+  );
+}
